f2b13dc411ef596ceec1c11fdfac4f4681feeba3
RJain7098 <rjain@stu.naperville203.org>
2019-10-24 10:57:31 -0500

    no changes were made why does it say there's a new commit

--- otherprogram.py
+++ otherprogram.py
@@ -4,6 +4,9 @@ print("Hello World!")
 def afunction(a):
     for i in range(a):
         print("git is great")
-        
+
+def add(a, b):
+    return a+b 
         
 afunction(10)
+print(add(4, 32))
