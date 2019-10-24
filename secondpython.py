1b5b4ff82e39d8fe30702770bf039aeb9b777b03
RJain7098 <rjain@stu.naperville203.org>
2019-10-24 11:05:08 -0500

    added second python file to the temp branch

---
+++ secondpython.py
@@ -0,0 +1,15 @@
+# Kyle
+print("Hello World!")
+
+def afunction(a):
+    for i in range(a):
+        print("git is great")
+
+def add(a, b):
+    return a+b 
+        
+
+def subtract(a, b):
+    return a-b; 
+afunction(10)
+print(add(4, 32))
